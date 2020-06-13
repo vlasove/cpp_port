61c380b80a4cbd9f583dcdb2237d078a70b4fc0d
EvgenyVlasov <evgeny_vlasov@yahoo.com>
2020-06-13 16:50:22 +0300

    started a4w1

--- yellow/week1/a4w1/a4w1/a4w1.cpp
+++ yellow/week1/a4w1/a4w1/a4w1.cpp
@@ -21,17 +21,61 @@ using TasksInfo = map<TaskStatus, int>;
 class TeamTasks {
 public:
     // Получить статистику по статусам задач конкретного разработчика
-    const TasksInfo& GetPersonTasksInfo(const string& person) const;
+    const TasksInfo& GetPersonTasksInfo(const string& person) const {
+        if (tasks_log.count(person) == 0) {
+            return {};
+        }
+        else {
+            return tasks_log.at(person);
+        }
+    }
 
     // Добавить новую задачу (в статусе NEW) для конкретного разработчитка
-    void AddNewTask(const string& person);
+    void AddNewTask(const string& person) {
+        ++tasks_log[person][TaskStatus::NEW];
+    }
 
     // Обновить статусы по данному количеству задач конкретного разработчика,
     // подробности см. ниже
-    tuple<TasksInfo, TasksInfo> PerformPersonTasks(
-        const string& person, int task_count);
+    tuple<TasksInfo, TasksInfo> PerformPersonTasks(const string& person, int task_count) {
+        if (tasks_log.count(person) == 0) {
+            return make_tuple(TasksInfo{}, TasksInfo{});
+        } 
+        if (task_count > TasksCounter(person)) {
+            task_count = TasksCounter(person);
+        }
+        auto status = TaskStatus::NEW;
+        TasksInfo updated_tasks, untouched_tasks;
+
+        for (int i = 0; i < task_count; i++) {
+            if (tasks_log[person][status] == 0) {
+                auto new_status = static_cast<TaskStatus>(static_cast<int>(status) + 1);
+                tasks_log[person].erase(status);
+                status = new_status;
+            }
+
+            --tasks_log[person][status];
+            ++tasks_log[person][static_cast<TaskStatus>(static_cast<int>(status) + 1)];
+            ++updated_tasks[static_cast<TaskStatus>(static_cast<int>(status) + 1)];
+        }
+
+        if (tasks_log[person].count(TaskStatus::NEW) != 0) {
+            untouched_tasks[TaskStatus::NEW] == tasks_log[person][TaskStatus::NEW];
+        }
+
+
+        return make_tuple(updated_tasks, untouched_tasks);
+            
+    }
 
 private:
+    int TasksCounter(const string& person) {
+        int result = 0;
+        for (const auto& t : tasks_log.at(person)) {
+            result += t.second;
+        }
+        return result;
+    }
     map<string, TasksInfo> tasks_log;
 };
 
